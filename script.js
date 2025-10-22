58f676cd095522ec8bd0bb56928f7b4c34113895
ritikj0536 <124515293+ritikj0536@users.noreply.github.com>
2025-10-22 11:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let TimerDisplay= document.getElementById("timer");
+let currentDate =new Date();
+set interval(S
+	function (){
+		currentDate=new Date();
+		TimerDisplay.innerHTML=currentDate.toLocaleString()
+	},1000
+)
